8ee72c71bc8526158b9bd01ca2f1c28c2b35cef4
Andres Martin <anrumara@gmail.com>
2018-01-18 00:38:23 -0500

    end of module 05 - POST and GET friends from API

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
+
+    $.ajax({
+        url: 'http://rest.learncode.academy/api/dreMartin/friends',
+        type: 'GET'
+    }).done( (data) => {
+        data.forEach( (eachFriend) => {
+            let name = eachFriend.first + " " + eachFriend.last;
+            let newElement = createFriend(name);
+            $('#friend-list').append(newElement);
+        })
+    }).fail( () => {
+        alert('AJAX call failed, unable to retrieve friends.');
+    })
+
+
+
     $('#friend-form').submit( (e) => {
         e.preventDefault();
         let newFriend = $('#input').val();
+        
+        let first = newFriend.substr(0, newFriend.indexOf(' '));
+        let last = newFriend.substr(newFriend.indexOf(' ')+1);
+        
+        $.ajax({
+            url: 'http://rest.learncode.academy/api/dreMartin/friends',
+            type: 'POST',
+            data: {
+                first: first,
+                last: last,
+                email: `${first}@${last}.com`
+            }
+        }).done( (data) => {
+            console.log(data)
+        }).fail( () => {
+            alert('AJAX call failed, unable to POST new friend.');
+        })
         let newElement = createFriend(newFriend);
         $('#friend-list').append(newElement);
         $('#input').val('');
     } )
-
+    
     $('#friend-list').on('click', 'li', function(e){
         $(this).remove();
     })
